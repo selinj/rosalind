a39a7bc7967d0503aa56f8068606e96fdc87369e
selinj <selinjessa@gmail.com>
2015-06-20 09:30:28 -0700

    Dictionaries

---
+++ PythonVillage/INI6.py
@@ -0,0 +1,15 @@
+'''
+
+Problem: Dictionaries
+ID: INI6
+URL: http://rosalind.info/problems/ini6/
+
+*dictionaries allow matching of one item (key) to a second (value)
+*you provide the key - like a non-numerical index (but has to be
+    an immutable type) - case-sensitive
+*value can be any type
+
+twitter = {'Selin':'@selinjessa', 'tester':'@test'}
+print twitter['Selin'] #would return my handle
+
+'''
