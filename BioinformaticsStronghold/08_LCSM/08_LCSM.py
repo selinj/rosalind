bc3bc80049aec250a41ceac8d0b2e33c65526ffb
selinj <selinjessa@gmail.com>
2015-06-20 19:24:02 -0700

    Finding a Shared Motif

---
+++ BioinformaticsStronghold/08_LCSM/08_LCSM.py
@@ -0,0 +1,17 @@
+
+'''
+
+
+given collection of k DNA strings in FASTA format
+return longest common substring - multiple, if that's the case
+
+'''
+
+f1 = open('rosalind_lcsm.txt','r')
+f2 = open('08_LCSMout.txt','w')
+
+test = f1.readline()
+seq1 = f1.readline()
+
+print seq1
+
