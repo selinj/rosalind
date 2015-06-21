8b09cc597fca98ecdd0851b3d0bfe76b2c756035
selinj <selinjessa@gmail.com>
2015-06-21 00:15:51 -0700

    Summary of the RNA Splicing problem

---
+++ BioinformaticsStronghold/10_SPLC/10_SPLC.py
@@ -0,0 +1,12 @@
+'''
+
+Given file w/ FASTA formats of seqs
+seq 1: string s
+seq 2 onwards: collection of substrings acting as introns
+
+Transcribe: splice out the introns, concatenate the exons into RNA
+
+return/Translate: protein string resulting from translating the RNA string
+    (= the exons of s)
+
+'''
