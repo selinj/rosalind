b8e29cb3eb61e39b684998c0b7ab879c02d1ea3b
selinj <selinjessa@gmail.com>
2015-06-21 14:59:53 -0700

    Enumerating k-mers Lexographically

---
+++ BioinformaticsStronghold/13_LEXF/13_LEXF.py
@@ -0,0 +1,32 @@
+﻿'''
+
+we organize genetic strings lexographically
+
+
+given a collection of <= 10 symbols defining an ordered alphabet, and int n
+return all strings of length n, ordered lexographically
+
+'''
+
+import itertools
+
+given = open('rosalind_lexf.txt','r')
+alphraw = str(given.readline().rstrip())
+alphabet = alphraw.split(' ')
+n = int(given.readline())
+perms = []
+
+for p in itertools.produßct(alphabet,repeat=n):
+    perms.append(p)
+
+#generalizing a format
+printformat = ''
+for i in range(0,n):
+    printformat += '{}'
+
+out = open('13_LEXFout.txt','w')
+
+for p in perms:
+    out.write(str(printformat.format(*p))+'\n')
+
+out.close()
