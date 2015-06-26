82279063db1a315111d6030e5648a59883855cda
selinj <selinjessa@gmail.com>
2015-06-25 22:32:53 -0700

    Bubble sort for custom alphabet

--- BioinformaticsStronghold/14_LEXV/14_LEXV.py
+++ BioinformaticsStronghold/14_LEXV/14_LEXV.py
@@ -10,45 +10,85 @@ return all strings of length n, ordered lexographically
 
 import itertools
 
-#given = open('rosalind_lexv.txt','r')
-#alphraw = str(given.readline().rstrip())
-#alphabet = alphraw.split(' ')
-#n = int(given.readline())
+given = open('rosalind_lexv.txt','r')
+alphraw = str(given.readline().rstrip())
+alphabet = alphraw.split(' ')
+n = int(given.readline())
 
 alphabet = ['D','N','A']
-n=3
+#n=3
 
 perms = []
 count = 0
+alphstring = ''
+
+for letter in alphabet:
+    alphstring += letter
+
+print alphstring
 
 for i in range(n+1):
-    print i
+    #print i
     for p in itertools.product(alphabet,repeat=i):
         perms.append(p)
         count +=1
 
-print perms
-print count
+#print perms
+#print count
 
-##for i in perms:
-##    for nt in i:
-##        
-def swap(a,b):
-    temp = ""
-    temp = a
-    a = b
-    b = temp
+#perms = [('D','A'),('A','A','A'),('N'),('D','D','D'),('N','A','D'),('N','D','A')]
 
+#print perms
 
-#generalizing a format
-#printformat1 = '{}'
+#a function to compare two variables by letter
+    
+def compare(p1,p2,orderedsymbols):
+    result = 0
+    length = 0
+    if len(p1) > len(p2) or len(p1) == len(p2):
+        length = len(p2)
+    elif len(p1) < len(p2):
+        length = len(p1)
+        
+    for j in range(length):
+        index1 = orderedsymbols.find(p1[j])
+        index2 = orderedsymbols.find(p2[j])        
+        if index1 == index2:
+            continue #check next letter
+        elif index1 < index2:
+            result = -1 #sorted
+            return result
+        elif index1 > index2:
+            result = 1 #not sorted
+            return result
+    if result == 0:
+        if len(p2) > len(p1):
+                result = -1 #second longer than first = sorted correctly
+        elif len(p1) > len(p2):
+                result = 1 #not sorted if second shorter than first
+        return result           
 
-#out = open('14_LEXVout.txt','w')
+def bubblesort(listtosort):
+    for i in range(1,len(listtosort)):
+        for j in range(i+1,len(listtosort)):
+            #print perms[i]
+            if compare(listtosort[i],listtosort[i+1],alphstring) == 1:
+                #then swap
+                listtosort[i],listtosort[i+1]=listtosort[i+1],listtosort[i]
+    return listtosort
 
-#for p in perms:
-#    if len(p)<n:
-        #perms[p].append(
-    #out.write(str(printformat.format(*p))+'\n')
-#        print printformat.format(*p)+'\n'
+for i in range(len(perms)):
+    bubblesort(perms)
+#print perms
+perms.pop(0)
 
-#out.close()
+printformat = ''
+
+out = open('14_LEXVout.txt','w')
+
+for p in perms:
+    print p
+    for i in range(1,len(p)):
+        printformat += '{}'
+    print printformat.format(*p)
+out.close()
