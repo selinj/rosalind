9ec4bd4d2198bafba902c66bfab75d1d654520c3
selinj <selinjessa@gmail.com>
2015-07-09 15:47:30 -0700

    Update

--- BioinformaticsStronghold/26_REVP/26_REVP.py
+++ BioinformaticsStronghold/26_REVP/26_REVP.py
@@ -16,40 +16,26 @@ data = SeqIO.parse(handle,'fasta').next()
 handle.close()
 
 dna = data.seq
-#revc = data.seq.reverse_complement()
-#revd = revc[::-1]
-
 #dna = Seq('TCAATGCATGCGGGTCTATATGCAT')
-#revc = dna.reverse_complement()
-#print dna
-#print revc
-#print revc[::-1]
-#revd = revc[::-1]
-#print revd
+rev_comp = dna.reverse_complement()
+revd = rev_comp[::-1]
 
-#check every substring of length between 4 and 12, if it equals its reverse complement
-def check_p(seq,start,n):
-    #s is the start index, n is the length
-    forward = seq[start:start+n]
-    revc = seq.reverse_complement()
-    revd = revc[::-1]
-    candidate = revd[start:start+n]
-    candidate = revd[::-1]
-    print forward
-    print candidate
-    return forward == candidate
+def check(i,n):
+    fwd, opp = dna[i:i+n], revd[i:i+n]
+    opp = opp[::-1]
+    return fwd == opp
 
+length = len(str(dna))
+print length
 out = open('26_REVPout.txt','w')
 
 for n in range(4,12):
-    print n
-    for i in range(len(dna)):
-        if check_p(dna,i,n):
-            print str(i) + ' ' + str(n)
-            #print str(dna[i:i+n])
-            #print str(revc[(i+n-1):(i-1):-1])
-            #out.write(str(i+1) + ' ' + str(n)+'\n')
-            #out.write(str(dna[i:i+n])+'\n')
-            #out.write(str(revc[i+n-1:i-1:-1])+'\n\n')
+    for i in range(length):
+        if check(i,n):
+            if len(str(dna[i:i+n])) >= n:
+                #print dna[i:i+n]
+                #print revd[i:i+n]
+                print str(i+1) + ' ' + str(n) #print i + 1 because rosalind uses 1-based numbering
+                out.write(str(i+1) + ' ' + str(n)+'\n')
 
 out.close()
---
+++ BioinformaticsStronghold/26_REVP/proofofconcept.py
@@ -0,0 +1,8 @@
+#proof of concept
+print "                   0123456789012345678901234"
+print "                   1234567890123456789012345"
+print "dna seq         5' " + dna + " 3'"
+rev_comp = dna.reverse_complement()
+print "rev comp        5' " + rev_comp + " 3'"
+revd = rev_comp[::-1]
+print "reversed r.c.   3' " + revd +  " 5'"
