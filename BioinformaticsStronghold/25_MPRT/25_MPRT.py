4d399a71526253641af492997b962186e958a266
selinj <selinjessa@gmail.com>
2015-07-09 12:31:52 -0700

    Clean up

--- BioinformaticsStronghold/25_MPRT/25_MPRT.py
+++ BioinformaticsStronghold/25_MPRT/25_MPRT.py
@@ -5,18 +5,26 @@ uniprot id B5ZC00'''
 import urllib2
 from Bio import SeqIO
 
+def getUniprotFile(protID):
+    output = urllib2.urlopen(("http://www.uniprot.org/uniprot/%s.fasta" % protID))
+    return output
+
+def find_indexes(protID,seq):
+    del listofindexes[:]
+    for i in range(len(seq)-5):
+        if seq[i] == 'N':
+            index = i
+            if seq[i+1] != 'P':
+                if seq[i+2] == 'S' or seq[i+2] == 'T':
+                    if seq[i+3] != 'P':
+                        listofindexes.append(i+1)
+    return listofindexes
+
 f1 = open('rosalind_mprt.txt','r')
 
 #make array of id's
 protList = []
 protList = [line.rstrip('\n') for line in f1]
-
-print "The list of IDs from Rosalind:"
-print protList
-
-def getUniprotFile(protID):
-    output = urllib2.urlopen(("http://www.uniprot.org/uniprot/%s.fasta" % protID))
-    return output
 
 protDict = {}
 for protID in protList:
@@ -28,27 +36,9 @@ for protID in protList:
     except:
         print "This didn't work."
 
-print "The Dictionary of IDs/UniProt sequences:"
-print protDict
-
 #N-glycosylation motif: N{P}[ST]{P}
 listofindexes = []
 index = 0
-
-def find_indexes(protID,seq):
-    print "Now checking protein "+protID
-    print "With sequence starting " +seq
-    del listofindexes[:]
-    for i in range(len(seq)-5):
-        if seq[i] == 'N':
-            index = i
-            if seq[i+1] != 'P':
-                if seq[i+2] == 'S' or seq[i+2] == 'T':
-                    if seq[i+3] != 'P':
-                        listofindexes.append(i+1)
-    print "Found N-glycosylation motif at "
-    print listofindexes
-    return listofindexes
 
 out = open('25_MPRTout.txt','w')
     
