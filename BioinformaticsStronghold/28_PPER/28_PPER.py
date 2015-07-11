a5278e6bfc0db59c480944cab459ef19c9547bdf
selinj <selinjessa@gmail.com>
2015-07-10 17:49:49 -0700

    Update w/ a few solutions

--- BioinformaticsStronghold/28_PPER/28_PPER.py
+++ BioinformaticsStronghold/28_PPER/28_PPER.py
@@ -20,6 +20,7 @@ return P(n,k) % 1 000 000
 '''
 import itertools
 import math
+from math import factorial
 import os
 #import time
 
@@ -27,18 +28,43 @@ import os
 #n,k = f1.readline().split()
 #n,k=int(n),int(k)
 
-n=86
-k=9
-#print n
-#print k
+n=81
+k=8
+print n
+print k
 
 count = 0
 nset = [i for i in range(1,n+1)]
-perms = sum(((count+1)) for p in itertools.permutations(nset,k)) % 1000000
-print perms
+
+#this works, but is too slow for Rosalind's 5-min answer windo
+#perms = sum(((count+1)) for p in itertools.permutations(nset,k)) % 1000000
+
+'''
+number of k permutations for n set
+= P(n,k) = n!/(n-k)!
+
+In factorial = n(n-1)(n-2)...(n-k-1)
+= 21,20,19...*13
+13,14,15...19,20,21
+'''
+
+result = 1
+for i in xrange(n-k-1,n+1):
+    print i
+    result *= i%1000000
+print result
+
+
+
+
+#print factorial(5)
+first = factorial(n)
+second = factorial(n-k)
+
+print (factorial(n)/factorial(n-k))%1000000
 
 #out = open('28_PPERout.txt','w')
 #out.write(str(count))
 #out.close()
 
-os.system('say "your program has finished"')
+#os.system('say "done!"')
