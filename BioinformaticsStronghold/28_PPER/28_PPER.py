a8ba9fa2a0af60e305b09249143d9f5025cdc03c
selinj <selinjessa@gmail.com>
2015-07-10 17:57:41 -0700

    Partial Permutations

--- BioinformaticsStronghold/28_PPER/28_PPER.py
+++ BioinformaticsStronghold/28_PPER/28_PPER.py
@@ -16,55 +16,32 @@ P(n,n) is simply the number of permutations of n objects
 given n, k
 return P(n,k) % 1 000 000
 
-
-'''
-import itertools
-import math
-from math import factorial
-import os
-#import time
-
-#f1 = open('rosalind_pper.txt','r')
-#n,k = f1.readline().split()
-#n,k=int(n),int(k)
-
-n=81
-k=8
-print n
-print k
-
-count = 0
-nset = [i for i in range(1,n+1)]
-
-#this works, but is too slow for Rosalind's 5-min answer windo
-#perms = sum(((count+1)) for p in itertools.permutations(nset,k)) % 1000000
-
-'''
 number of k permutations for n set
 = P(n,k) = n!/(n-k)!
 
 In factorial = n(n-1)(n-2)...(n-k-1)
 = 21,20,19...*13
 13,14,15...19,20,21
+
 '''
 
-result = 1
-for i in xrange(n-k-1,n+1):
-    print i
-    result *= i%1000000
-print result
+import itertools
+import math
+from math import factorial
+import os
 
+f1 = open('rosalind_pper.txt','r')
+n,k = f1.readline().split()
+n,k=int(n),int(k)
 
-
-
-#print factorial(5)
-first = factorial(n)
-second = factorial(n-k)
+#count = 0
+#this works, but is too slow for Rosalind's 5-min answer windo
+#nset = [i for i in range(1,n+1)]
+#perms = sum(((count+1)) for p in itertools.permutations(nset,k)) % 1000000
 
 print (factorial(n)/factorial(n-k))%1000000
+perms = (factorial(n)/factorial(n-k))%1000000
 
-#out = open('28_PPERout.txt','w')
-#out.write(str(count))
-#out.close()
-
-#os.system('say "done!"')
+out = open('28_PPERout.txt','w')
+out.write(str(perms))
+out.close()
